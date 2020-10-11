af83338c8ccfc110d34241e8cdf7d2b99a56ca44
sloth143chunk <68003372+sloth143chunk@users.noreply.github.com>
2020-10-11 15:28:53 -0700

    adjusting trees

--- Mapping_Single_Point/static/js/logic.js
+++ Mapping_Single_Point/static/js/logic.js
@@ -2,9 +2,6 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
       40.7, -94.5
@@ -12,22 +9,8 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
-//   // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
 //  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 var circle = L.circle([34.0522, -118.2437], {
-// var circle = L.circleMarker([34.0522, -118.2437], {
   color: 'black',
   radius: 300,
   fillColor: 'yellow'
